b0a0f801ffa9ed5c5298d0d1254e32d512fc7cda
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2015-02-18 16:04:43 -0500

    Moved demo filters out of angular-tree-autocomplete core

--- demo/app.js
+++ demo/app.js
@@ -1,5 +1,70 @@
 angular.module('AngularAutocompleteDemo', ['ui.bootstrap', 'angularTreeAutocomplete'])
 
+.filter('DemoSiblings', ['$q', '$filter', function($q, $filter) {
+    return function(query, source) {
+        var filterDeferred = $q.defer();
+
+        var nameSearch = new RegExp(query.trim(), "gi");
+        var idSearch = new RegExp('^' + query.trim());
+        filterDeferred.resolve($filter('filter')(source, function(obj, index) {
+            var name_match = nameSearch.test(obj.name);
+            var id_match = idSearch.test(obj.id);
+            return name_match || id_match;
+        }, false));
+
+        return filterDeferred.promise;
+    }
+}])
+
+.filter('DemoChildren', ['$q', '$filter', function($q, $filter) {
+    return function(query, source) {
+        var filterDeferred = $q.defer();
+        var filterSet = source.children.filter(function(child) {
+            return child.type === 'BLK';
+        });
+
+        var nameSearch = new RegExp(query.trim(), "gi");
+        var idSearch = new RegExp('^' + query.trim());
+        filterDeferred.resolve($filter('filter')(filterSet, function(obj, index) {
+            var name_match = nameSearch.test(obj.name);
+            var id_match = idSearch.test(obj.id);
+            return (name_match || id_match)
+
+        }, false));
+
+        return filterDeferred.promise;
+    }
+}])
+
+.filter('DemoNamespaces', ['$q', '$filter', function($q, $filter) {
+    return function(query, source) {
+        var filterDeferred = $q.defer();
+        var query = query.split('.');
+        if (query.length === 1) {
+            $filter('DemoSiblings')(query[0], source).then(function(results) {
+                filterDeferred.resolve(results);
+            });
+        } else if (query.length > 1) {
+            $filter('DemoSiblings')(query[0], source).then(function(results) {
+                var childSet = results[0].children.filter(function(child) {
+                    return child.type === 'ATTR';
+                });
+
+                var nameSearch = new RegExp(query[1].trim(), "gi");
+                var idSearch = new RegExp('^' + query[1].trim());
+
+                filterDeferred.resolve($filter('filter')(childSet, function(obj, index) {
+                    var name_match = nameSearch.test(obj.name);
+                    var id_match = idSearch.test(obj.id);
+                    return (name_match || id_match);
+                }, false));
+            });
+        }
+
+        return filterDeferred.promise;
+    }
+}])
+
 .controller('AutocompleteCtrl', function($scope) {
     console.log('Loaded AutocompleteCtrl');
     $scope.treeDemo = [
